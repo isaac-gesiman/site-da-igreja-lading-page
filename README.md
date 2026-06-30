b7d79b226702dadb0704771142e3bebda77cb34f
isaac-gesiman <isaac.rodmed@gmail.com>
2026-06-30 11:53:49 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Lading-page-igreja-baatista-getsemani
---
+++ index.js
@@ -0,0 +1,5 @@
+function mudarStyle() {
+    const el = document.getElementById("tema");
+    el.link.href = "stylePreto";
+    alert("Estilo alterado para preto");
+}
