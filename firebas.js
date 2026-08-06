e184d30c9cb8b51eac677cdb87fff2053bee2e0d
isaac-gesiman <isaac.rodmed@gmail.com>
2026-08-06 12:13:04 -0300

    First commit

--- firebas.js
+++ firebas.js
@@ -16,6 +16,6 @@ const firebaseConfig = {
   measurementId: "G-999G05XHBH"
 };
 
-// Initialize Firebase
+// Initialize Firebase saSas
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
